bd38422f1f85e4fbe37bb28c698fbdc64d746d5b
andika.dwiki <andika.dwiki@sucofindo.co.id>
2025-08-10 12:55:12 +0700

    refactor(products): remove isActive filter from product GET endpoint

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -20,7 +20,6 @@ export async function GET(
     const product = await prisma.product.findUnique({
       where: {
         id: productId,
-        isActive: true,
       },
       include: {
         category: true,
